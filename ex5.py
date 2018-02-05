6fd8f650af3caf4d4d97d19dfc1504ffb63b2cc2
cybagesamidha <dubey.samidha755@gmail.com>
2018-02-05 17:43:22 +0530

    second commit

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Samidha'
+my_age = 24 # not lie
+my_hight = 74
+my_weight = 54
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+print "let's talk about %s." % my_name
+print " she's %d iches tall." % my_hight
+print " she's %d pound heavy." % my_weight
+print "Actually that's not too heavy"
+print "she's got %s eyes and %s hair" %(my_eyes, my_hair)
+print "her teeth are usually %s deepending on the coffe." % my_teeth
+
+# this is tricky, try to get it exactly right.
+print "if i add %d, %d, and %d i get %d." %(my_age, my_hight, my_weight, my_age + my_hight + my_weight)
